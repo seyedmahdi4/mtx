a229714027704fa18c6df8944943d976307f5c1b
aghaseyed1 <aghaseyed7@gmail.com>
2023-03-29 02:20:32 +0330

    very alpha2

--- main.py
+++ main.py
@@ -28,40 +28,61 @@ class NPC:
 
     def eat(self):
         self.status = stats["eating"]
-        self.time_for_change_status = 10
+        self.time_for_change_status = 10  # TODO : not hardcode
 
     def sleep(self):
         self.status = stats["sleeping"]
-        self.time_for_change_status = 100
+        self.time_for_change_status = 100  # TODO : not hardcode
 
     def idle(self):
         self.status = stats["idle"]
         self.time_for_change_status = 0
 
     def decrease_hunger(self):
-        self.hunger -= hunger_decrease_per_tick
+        if self.hunger >= 0:
+            self.hunger -= hunger_decrease_per_tick
+        else:
+            self.hunger = 0
+            # TODO: not hardcode
+            self.decrease_energy(3*energy_decrease_per_tick)
 
     def decrease_hunger_when_sleep(self):
-        self.hunger -= hunger_decrease_per_tick_sleep_time
+        if self.hunger <= 20:
+            self.idle()
+        else:
+            self.hunger -= hunger_decrease_per_tick_sleep_time
 
-    def decrease_energy(self):
-        self.energy -= energy_decrease_per_tick
+    def decrease_energy(self, n=energy_decrease_per_tick):
+        if self.energy - energy_decrease_per_tick <= 0:
+            # TODO: decrease helth
+            self.energy = 0
+        else:
+            self.energy -= n
 
     def increase_hunger(self):
-        self.hunger += get_hunger_from_food 
+        if self.hunger + get_hunger_from_food >= self.max_hunger:
+            self.hunger = self.max_hunger
+            self.idle()
+        else:
+            self.hunger += get_hunger_from_food
 
     def increase_energy_from_sleep(self):
-        self.energy += get_energy_from_sleep
+        if self.energy + get_energy_from_sleep >= self.max_energy:
+            self.energy = self.max_energy
+        else:
+            self.energy += get_energy_from_sleep
 
     def increase_energy_from_food(self):
-        self.energy += get_energy_from_food
+        if self.energy + get_energy_from_food >= self.max_energy:
+            self.energy = self.max_energy
+        else:
+            self.energy += get_energy_from_food
 
     def decrease_status_time(self):
         self.time_for_change_status -= 1
 
-
     def __str__(self):
-        return f"Status: {self.status}\nTime for change status:{self.time_for_change_status}\nHonger: {self.hunger}\nEnergy: {self.energy}\nHealth: {self.health}"
+        return f"Status: {self.status}\nTime for change status:{self.time_for_change_status}\nHonger: {round(self.hunger , 2 )}\nEnergy: {round(self.energy , 2)}\nHealth: {self.health}"
 
 
 npc1 = NPC()
@@ -72,20 +93,20 @@ def main():
     # check status
     for npc in NPCs:
         # TODO: helth check and increase
-        if npc.status !=0 and npc.time_for_change_status != 0:
+        if npc.status != 0 and npc.time_for_change_status != 0:
             npc.decrease_status_time()
             if npc.time_for_change_status == 0:
                 npc.idle()
 
         if npc.status == 1:  # sleeping
-            npc.decrease_hunger_when_sleep()  # TODO: if not 0
-            npc.increase_energy_from_sleep()  # TODO: if not max
+            npc.decrease_hunger_when_sleep()
+            npc.increase_energy_from_sleep()
         else:
-            npc.decrease_hunger()  # TODO: if not 0
-            npc.decrease_energy()  # TODO: if not 0
+            npc.decrease_hunger()
+            npc.decrease_energy()
             if npc.status == 2:  # eating
-                npc.increase_energy_from_food()  # TODO: if not max
-                npc.increase_hunger()  # TODO: if not max
+                npc.increase_energy_from_food()
+                npc.increase_hunger()
             elif npc.status == 0:  # idle
                 if npc.hunger < 20:  # TODO: not hardcode
                     npc.eat()
@@ -99,3 +120,4 @@ while 1:
         os.system("clear")
         print(npc1)
         main()
+# TODO round
